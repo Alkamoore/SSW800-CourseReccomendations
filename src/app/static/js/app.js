1e8fe7a0142e9f0625fe3e2b61463f21b9b39e0b
Alexis Moore <alkamoore@gmail.com>
2017-03-04 15:56:38 -0500

    Fixed Angular Template
    Fixed angular template to allow multiple pages. Need to re-add calendar

--- src/app/static/js/app.js
+++ src/app/static/js/app.js
@@ -1,17 +1,6 @@
 //inheriting ngMaterial from Google, and overlay.js
 
 $(document).ready(function () {
-    // page is now ready, initialize the calendar..
-    $('#calendar').fullCalendar({
-        // put your options and callbacks here
-        defaultView: 'agendaWeek',
-        columnFormat: 'dddd',
-        weekends: false,
-        minTime: '08:00:00',
-        maxTime: '21:00:00',
-        header: false,
-        height: "parent",
-    });
 });
 
 
@@ -19,124 +8,28 @@ $(document).ready(function () {
     'use strict';
 
     angular.module('scheduler', ['ngMaterial', 'ngDialog'])
-        .controller('cTabModule', cTabModule)
-        .controller('cToggleNavigation', cToggleNavigation)
-        .controller('cSideCtrl', cSideCtrl)
-        .controller('cOverlayCtrl', cOverlayCtrl)
-        .directive('tabNavigation', tabNavigation);
-
-    //tab module for optimal class selections
-    function cTabModule($scope, $log, $rootScope) {
-        $scope.tabs = [];
-        $scope.selectedIndex = 0;
-        $rootScope.selectedOption = 0;
-
-        $scope.$watch('selectedIndex', function (current, old) {
-            $rootScope.selectedOption = current - 1;
-            $rootScope.$emit('onOptionChange');
-        });
-
-        $rootScope.$on('setTabCount', function (event, args) {
-            $log.info('cTabModule.onEvent:' + args);
-            $scope.tabs = [];
-            for (var i = 0; i < parseInt(args); i++) {
-                $scope.addTab('Option ' + (i + 1));
-            }
-            $scope.$apply();
-        });
-
-        $scope.addTab = function (title) {
-            $log.info('title:' + title);
-            $scope.tabs.push({title: title, disabled: false});
-        };
-
-        $scope.removeTab = function (tab) {
-            var index = tabs.indexOf(tab);
-            tabs.splice(index, 1);
-        };
-
-    }
-
-    //Toggles side Navigation bar on and off
-    function cToggleNavigation($scope, $mdSidenav, $log) {
-        $scope.toggleLeft = buildToggler('left');
-
-        $scope.toggleLeft = function () {
-            return $mdSidenav('left').toggle();
-        };
-
-        $scope.isSidebarOpen = function () {
-            return $mdSidenav('left').isOpen();
-        };
-
-        function buildToggler(componentId) {
-            return function () {
-                $mdSidenav(componentId).toggle();
-            }
-        }
-
-        $scope.close = function () {
-            // Component lookup should always be available since we are not using `ng-if`
-            $mdSidenav('left').close()
-                .then(function () {
-                    $log.debug("close LEFT is done");
-                });
-        };
-    }
-
-    //Side control options Scheduler/Courses
-    function cSideCtrl($scope, $log, $rootScope, ngDialog) {
-        var self = this;
-        self.nav = 'scheduler';
-		$rootScope.body = 'scheduler';
-        self.searchTimeout = false;
-
-        self.semesters = [
-            {id: '2017S', name: 'Spring 2017'},
-            {id: '2016F', name: 'Fall 2016'}
-        ];
-
-        self.selectedSemester = self.semesters[0];
-
-        self.setSemester = function () {
-            $log.info('setSemester:' + self.selectedSemester.name);
-            $.get("/api/schedule/tree", {semester: self.selectedSemester.id}, function (data) {
-                var tree = $('#schedule_tree');
-                tree.jstree(true).settings.core.data = data.results;
-                tree.jstree(true).uncheck_all();
-                tree.jstree(true).refresh();
-            });
-        };
-
-        self.selectedItemChange = function (text) {
-            //$log.info('Text changed to ' + text);
-        };
-
-        self.searchTextChange = function (item) {
-            //$log.info('Item changed to ' + JSON.stringify(item));
-        };
-
-        self.setNav = function (page) {
-            console.log("nav = " + page);
-			console.log("body = " + page);
-			$rootScope.body = page;
-            self.nav = page;
-			
-        };
+        .controller('cNavigation', cNavigation)
+        .controller('cSideCtrl', cSideCtrl);
 		
-		$rootScope.getBody = function () {
-            return $rootScope.body;
-        };
+	angular.module('scheduler').directive('cCourseBody',  function() { return {
+		templateUrl: '/static/html/reccomendations.html'
+	}});
 	
-        self.getNav = function () {
-            return self.nav;
-        };
+	angular.module('scheduler').directive('cLoginBody',  function() { return {
+		templateUrl: '/static/html/login.html'
+	}});
+	
+	angular.module('scheduler').directive('cCalendarBody',  function() { return {
+		templateUrl: '/static/html/calendar.html'
+	}});
 
-        self.clearSelected = function () {
-            $('#schedule_tree').jstree(true).deselect_all();
-        };
-
-        self.searchTree = function (searchString) {
+	angular.module('scheduler').directive('cSearch', function() { return {
+		templateUrl: '/static/html/search.html',
+		controllerAs: 'search',
+		controller: function() {
+			var self = this;
+			self.searchTimeout = false;
+			self.searchTree = function (searchString) {
             if (self.searchTimeout) {
                 clearTimeout(self.searchTimeout);
             }
@@ -145,9 +38,17 @@ $(document).ready(function () {
                 //$('#course_tree').jstree(true).search(searchString);
             }, 300);
         };
-
-
-        $('#course_tree')
+		        self.clearSelected = function () {
+            $('#schedule_tree').jstree(true).deselect_all();
+        };
+		
+		}
+	}});
+	
+		angular.module('scheduler').directive('cCourseInfo', function() { return {
+		templateUrl: '/static/html/course_info.html',
+		/**@ngInject**/controller: function(ngDialog) {
+			$('#course_tree')
             .on('changed.jstree', function (e, data) {
                 var i, j, r = [];
                 for (i = 0, j = data.selected.length; i < j; i++) {
@@ -164,7 +65,7 @@ $(document).ready(function () {
 
                         console.log(data);
                         var dialog = ngDialog.open({
-                            template: '<div ng-controller="cOverlayCtrl as overlay">' +
+                            template: '<div>' +
                             '<h2>Course Info:</h2>' +
                             '<div><p><b>Name:</b> ' + data.name + '</p><p><b>Course Number: </b>' + data.letter + ' ' + data.number + '</p></div>' +
                             '<div><p><b>Credits:</b> ' + data.credits + '</p></div>' +
@@ -200,74 +101,99 @@ $(document).ready(function () {
                 },
                 "plugins": ["search"]
             });
+		}
+	}});
+	
 
-        $rootScope.$on('onOptionChange', function (event) {
-            if ($rootScope.selectedOption > -1) {
-                $log.info('onOptionChange');
-                var calendar = $('#calendar');
-                calendar.fullCalendar('removeEvents');
-                calendar.fullCalendar('removeEventSources');
-                calendar.fullCalendar('addEventSource', $rootScope.calendarData[$rootScope.selectedOption]);
-                calendar.fullCalendar('refetchEvents');
-                calendar.fullCalendar('rerenderEvents');
-                $log.info('setting event source to');
-                $log.info($rootScope.calendarData[$rootScope.selectedOption]);
+    //tab module for optimal class selections
+    function cTabModule($scope, $log, $rootScope) {
+        $scope.tabs = [];
+        $scope.selectedIndex = 0;
+        $rootScope.selectedOption = 0;
+
+        $scope.$watch('selectedIndex', function (current, old) {
+            $rootScope.selectedOption = current - 1;
+            $rootScope.$emit('onOptionChange');
+        });
+
+        $rootScope.$on('setTabCount', function (event, args) {
+            $log.info('cTabModule.onEvent:' + args);
+            $scope.tabs = [];
+            for (var i = 0; i < parseInt(args); i++) {
+                $scope.addTab('Option ' + (i + 1));
             }
+            $scope.$apply();
         });
 
-        var schedule_tree = $('#schedule_tree');
+        $scope.addTab = function (title) {
+            $log.info('title:' + title);
+            $scope.tabs.push({title: title, disabled: false});
+        };
 
-        function getSelectedCallNumbers() {
-            var r = [];
-            $.each(schedule_tree.jstree("get_checked", true), function () {
-                if (this.a_attr['call-number']) {
-                    r.push(this.a_attr['call-number']);
-                }
-            });
-            return r
+        $scope.removeTab = function (tab) {
+            var index = tabs.indexOf(tab);
+            tabs.splice(index, 1);
+        };
+
+    }
+
+    //Toggles side Navigation bar on and off
+    function cNavigation($scope, $mdSidenav, $log) {
+        $scope.toggleLeft = buildToggler('left');
+
+        $scope.toggleLeft = function () {
+            return $mdSidenav('left').toggle();
+        };
+
+        $scope.isSidebarOpen = function () {
+            return $mdSidenav('left').isOpen();
+        };
+
+        function buildToggler(componentId) {
+            return function () {
+                $mdSidenav(componentId).toggle();
+            }
         }
+    }
 
+    //Side control options Scheduler/Courses
+    function cSideCtrl($scope, $log, $rootScope, ngDialog) {
+        var self = this;
+        self.nav = 'course_info';
+		$rootScope.body = 'course_info';
 
-        schedule_tree.on('changed.jstree', function (e, data) {
-            $.post('/api/schedule/combinations', {
-                'semester': self.selectedSemester.id,
-                'call_numbers': getSelectedCallNumbers()
-            }).done(function (data) {
-                $rootScope.calendarData = data;
-                $rootScope.$emit('setTabCount', $rootScope.calendarData.length);
-                var calendar = $('#calendar');
-                calendar.fullCalendar('removeEvents');
-                calendar.fullCalendar('removeEventSources');
-                calendar.fullCalendar('addEventSource', $rootScope.calendarData[$rootScope.selectedOption]);
-                calendar.fullCalendar('refetchEvents');
-                calendar.fullCalendar('rerenderEvents');
+        self.semesters = [
+            {id: '2017S', name: 'Spring 2017'},
+            {id: '2016F', name: 'Fall 2016'}
+        ];
+
+        self.selectedSemester = self.semesters[0];
+
+        self.setSemester = function () {
+            $log.info('setSemester:' + self.selectedSemester.name);
+            $.get("/api/schedule/tree", {semester: self.selectedSemester.id}, function (data) {
+                var tree = $('#schedule_tree');
+                tree.jstree(true).settings.core.data = data.results;
+                tree.jstree(true).uncheck_all();
+                tree.jstree(true).refresh();
             });
+        };
 
-        }).jstree({
-            'core': {
-                'data': function (obj, cb) {
-                    $.get("/api/schedule/tree", {semester: self.selectedSemester.id}, function (data) {
-                        cb.call(this, data.results);
-                    });
-                },
-                'themes': {
-                    'icons': false
-                },
-            },
-
-            "search": {
-                "case_insensitive": true,
-                "show_only_matches": true,
-                "multiple": false
-            },
-            "checkbox": {
-                "keep_selected_style": false,
-                "check_callback": true
-            },
-            "plugins": ["search", "checkbox"],
-
-        });
-
+        self.setNav = function (page) {
+            console.log("nav = " + page);
+			console.log("body = " + page);
+			$rootScope.body = page;
+            self.nav = page;
+			
+        };
+		
+		$rootScope.getBody = function () {
+            return $rootScope.body;
+        };
+	
+        self.getNav = function () {
+            return self.nav;
+        };
 
         /**
          * Create filter function for a query string
@@ -280,24 +206,6 @@ $(document).ready(function () {
                     (item.name.toLowerCase().indexOf(lowercaseQuery) === 0);
             };
         }
-    }
-
-    // overlay controller
-    function cOverlayCtrl($scope, $log) {
-        var self = this;
-        self.selectCourseCb = function (course_letter, course_number) {
-            $log.info('selectCourseCb:' + course_letter + course_number);
-            $('#schedule_tree').jstree(true).select_node(course_letter + ' ' + course_number);
-        };
-    }
-
-    //Angular Element Directives
-    function tabNavigation($log) {
-        $log.info("tabNavigationFunction");
-        return {
-            restrict: 'E',
-            templateUrl: 'static/html/tabNavigation.html'
-        };
     }
 
 })();
